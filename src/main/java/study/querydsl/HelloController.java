02708ca4d491897fa847b13bd6e781fa82938d3e
Kim Young Jin <wndrhtkfurh@naver.com>
2021-11-17 06:54:09 +0900

    Commit

---
+++ src/main/java/study/querydsl/HelloController.java
@@ -0,0 +1,15 @@
+package study.querydsl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("HelloController.hello");
+
+        return "hello";
+    }
+}
